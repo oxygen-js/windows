b177395ac8a6620926bcef807b2d88666f408ff3
oxygen-js <89063820142ka@gmail.com>
2020-08-26 17:05:19 +0300

    Верстка формы авторизации

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -6,14 +6,38 @@ const Authorization:React.FC = () => {
         <div className="window-authorization">
             <div className="window">
                 <div className="title-bar">
-                    <div className="title-bar-text">Authorization</div>
-                    <div className="title-bar-controls">
-                        <button aria-label="Close"></button>
+                    <div className="title-bar-text">
+                        Log On to Windows
                     </div>
                 </div>
+                <div className="window-authorization_bg">
+
+                </div>
                 <div className="window-body">
-                    <div className="field-row-stacked">
-                        <input id="text22" type="text" />
+                    <form className="field-row-stacked window-authorization__form">
+                        <span>
+                            <span>
+                                User name:
+                            </span>
+                            <input id="text22" type="text" />
+                        </span>
+                        <span>
+                            <span>
+                                Password:
+                            </span>
+                            <input id="text223" type="password" />
+                        </span>
+                    </form>
+                    <div className="window-authorization__btns">
+                        <button className="focused">
+                            Ok
+                        </button>
+                        <button>
+                            Cancel
+                        </button>
+                        <button>
+                            Options >>
+                        </button>
                     </div>
                 </div>
             </div>
